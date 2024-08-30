7d89c0ef08fc714d22498d97062f0e94aa08a4a0
boxdate <88315712+boxdate@users.noreply.github.com.>
2024-08-31 00:03:32 +0900

    UpdatedTestConditionLogic

--- python/project01/extract_conditions.py
+++ python/project01/extract_conditions.py
@@ -19,9 +19,18 @@ class ConditionExtractor:
             content = file.read()
             
         #if分の条件を正規表現で抽出
-        conditions = re.findall(r'if\s\((.*?)\)', content)
+        #複数条件の抽出ができるように変更
+        conditions = re.findall(r'if\s*\(([^)]+)\)', content)
         return conditions
-
+    
+    def split_conditions(self, condition):
+        # 論理演算子で条件を分割する
+        return re.split(r'\s*&&\s*\|\s*\|\|\s', condition)
+    
+    def extract_coparisons(self, sub_condition):
+        # 各条件を分割して解析する
+        return re.findall(r'(\w+)\s*([><]=?)\s*(\d+)', sub_condition)
+    
 # 使用例
 if __name__ == "__main__":
     activity_diagram_text_file_path = 'output/activity_diagram.txt'  # 出力txtファイルのパス
